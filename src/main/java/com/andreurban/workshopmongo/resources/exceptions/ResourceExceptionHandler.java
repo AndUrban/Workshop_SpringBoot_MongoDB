4e2369438753d153bd4c078295b7f03be0a27dbe
Andre Urban <andreurbanwork@gmail.com>
2024-10-05 15:10:59 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/andreurban/workshopmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.andreurban.workshopmongo.resources.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.andreurban.workshopmongo.services.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandartError> ObjectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandartError err = new StandartError(System.currentTimeMillis(), status.value(), "Não encpntrado",
+				e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
