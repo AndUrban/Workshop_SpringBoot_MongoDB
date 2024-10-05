0b6bbc2bdb922e13eb8990d8a55e10246bd936b3
Andre Urban <andreurbanwork@gmail.com>
2024-10-05 15:34:17 -0300

    Insercao de usuario com POST

--- src/main/java/com/andreurban/workshopmongo/services/UserService.java
+++ src/main/java/com/andreurban/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andreurban.workshopmongo.domain.User;
+import com.andreurban.workshopmongo.dto.UserDTO;
 import com.andreurban.workshopmongo.repository.UserRepository;
 import com.andreurban.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDto(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
